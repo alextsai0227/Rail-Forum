93022b5666a2b20673912b777627f9ae7ebdfcaa
Alex T <alextsai0227@gmail.com>
2017-08-15 23:49:18 +0800

    using scope to write order by created_at to group's post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :group
   validates :content, presence: true
+  # Rails 內建API scope  
+  # example post.recent 就等於 post.order("created_at DESC")
+  scope :recent, -> { order("created_at DESC")}
 end
